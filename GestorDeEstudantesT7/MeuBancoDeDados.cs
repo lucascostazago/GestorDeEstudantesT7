43108771c388a27ddd70110c0ed48337e74c006f
EvertonHimself <evertondeoliveira@gmail.com>
2024-05-06 15:23:05 -0300

    Criada a conexão com o banco de dados.
    Criada a conexão com o banco de dados e importada a DLL do MySQL Connector.

---
+++ GestorDeEstudantesT7/MeuBancoDeDados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GestorDeEstudantesT7
+{
+    internal class MeuBancoDeDados
+    {
+        // A conexão com o Banco de Dados.
+        MySqlConnection conexao = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=sga_estudantes_bd_t7");
+    }
+}
